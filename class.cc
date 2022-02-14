58444ef3f2ea2e1fc0463209ad533d2cd8850585
Dilraj Bal <dilraj.bal@hotmail.com>
2022-02-13 20:42:17 -0800

    Updated comments describing class anatomy

--- class.cc
+++ class.cc
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <algorithm>
 using namespace std;
 
 class Rectangle
@@ -7,6 +8,8 @@ class Rectangle
     private:
         int length;
         int breadth;
+        int a;
+        int *p;
 
 
     public:
@@ -15,20 +18,30 @@ class Rectangle
         {
             length = 1;
             breadth = 1;
+            a = 1;
         }
 
         //paramaterized constructor
-        Rectangle(int l=1, int b=1)
+        Rectangle(int l=1, int b=1, int a=1)
         {
             setLength(l);
             setBreadth(b);
+            a = a;
+            p = new int[a];
+            std::fill_n(p, a, 1);
+        }
+
+        Rectangle(Rectangle &t){
+            a = t.a;
+            p = new int[a];
+
         }
         // Rectangle(Rectangle &rect)
         // {
         //     length = rect.length();
         //     breadth=rect.breadth();
         // }
-
+        //Property Functions
         //Mutators:
         // Check if the input variable is valid
         void setLength(int l){
@@ -60,15 +73,26 @@ class Rectangle
         void getbreadth(){
             std::cout << "Breadth value is: "<< breadth<<endl;
         }
+
+        void getParray(){
+            std::cout<<"Values in array p: "<<p<<endl;
+        }
+
+        //facilitator function
         int area(){
             return length * breadth;
         }
+
+        //inquiry
+
+        //destructors
 };
 
 
 int main()
 {
-    Rectangle r1(2,2);
+    Rectangle r1(2,2,2);
+    Rectangle r2(r1);
     std::cout<<r1.area()<<endl;
 
     return 0;
